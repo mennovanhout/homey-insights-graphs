8acc48ee62ef0069702b84fa6593bda7cdce78c3
Menno van Hout <info@mennovanhout.nl>
2023-08-12 12:50:02 +0200

    Add bar type configuration, UI improvements

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import Homey, {FlowCard, Image} from 'homey';
 import { HomeyAPI, HomeyAPIV3Local } from 'homey-api';
 import ChartJSImage from "chart.js-image";
+import LogNormaliser, {Log} from './src/LogNormaliser';
 
 class InsightGraphs extends Homey.App {
   resolutionSelection: string[] = ['lastHour', 'last6Hours', 'last24Hours', 'last7Days', 'last14Days', 'last31Days',
@@ -21,27 +22,29 @@ class InsightGraphs extends Homey.App {
     this.deviceManager = this.homeyApi!.devices;
 
     this.createGraphActionCard = this.homey.flow.getActionCard('create-graph-image');
-    this.createGraphActionCard.registerArgumentAutocompleteListener('type', this.autocompleteListener.bind(this));
+    this.createGraphActionCard.registerArgumentAutocompleteListener('device', this.autocompleteListener.bind(this));
     this.createGraphActionCard.registerRunListener(this.runListener.bind(this));
   }
 
   private async runListener(args: any, stats: any): Promise<{graph: Image}> {
     // Get logs
-    const logs = await this.insightsManager!.getLogEntries({id: args.type.id, uri: args.type.uri, resolution: args.resolution});
+    const logs: Log = await this.insightsManager!.getLogEntries({id: args.device.id, uri: args.device.uri, resolution: args.resolution});
 
-    this.log(logs);
+    const logNormaliser = new LogNormaliser(logs, args.resolution);
+    const values = logNormaliser.getNormalisedLogs();
 
     // Generate images
     // @ts-ignore
     const chart = new ChartJSImage().chart({
-      type: "line",
+      type: args.type,
       data: {
-        labels: logs.values.map((log: any) => this.formatLabel(log.t, args.resolution)),
+        labels: values.map((log) => log.t),
         datasets: [
           {
-            label: `${args.type.name} - ${args.resolution}`,
-            data: logs.values.map((log: any) => log.v),
-            backgroundColor: args.backgroundColor,
+            label: `${args.device.name} - ${args.resolution}`,
+            data: values.map((log) => log.v),
+            borderColor: args.lineColor,
+            backgroundColor: `#${this.addAlpha(args.lineColor.replace('#', ''), 0.2)}`,
           }
         ]
       },
@@ -52,6 +55,9 @@ class InsightGraphs extends Homey.App {
               beginAtZero: false
             }
           }]
+        },
+        chartArea: {
+          backgroundColor: args.backgroundColor
         }
       }
     });
@@ -64,58 +70,10 @@ class InsightGraphs extends Homey.App {
     };
   }
 
-  private formatLabel(label: string, resolution: string): string {
-    const date = new Date( Date.parse(label));
-    let formattedDate = '';
-
-    switch (resolution) {
-      default:
-      case 'lastHour':
-      case 'last6Hours':
-      case 'last24Hours':
-      case 'yesterday':
-      case 'today':
-        formattedDate = `${('0'+date.getHours()).slice(-2)}:${('0'+date.getMinutes()).slice(-2)}`;
-        break;
-      case 'last7Days':
-      case 'last14Days':
-      case 'last31Days':
-      case 'lastMonth':
-      case 'thisMonth':
-        formattedDate = `${('0'+date.getDate()).slice(-2)}/${('0'+(date.getMonth()+1)).slice(-2)}`;
-        break;
-      case 'thisWeek':
-      case 'lastWeek':
-        formattedDate = `${this.dayToWeekday(date.getDay())}`;
-        break;
-      case 'thisYear':
-      case 'lastYear':
-      case 'last2Years':
-        formattedDate = `${('0'+date.getDate()).slice(-2)}/${('0'+(date.getMonth()+1)).slice(-2)}/${date.getFullYear()}`;
-    }
-
-    return formattedDate;
-  }
-
-  private dayToWeekday(day: number): string {
-    switch (day) {
-      case 0:
-        return 'Sunday';
-      case 1:
-        return 'Monday';
-      case 2:
-        return 'Tuesday';
-      case 3:
-        return 'Wednesday';
-      case 4:
-        return 'Thursday';
-      case 5:
-        return 'Friday';
-      case 6:
-        return 'Saturday';
-      default:
-        return '';
-    }
+  private addAlpha(color: string, opacity: number) {
+    // coerce values so ti is between 0 and 1.
+    const  _opacity = Math.round(Math.min(Math.max(opacity || 1, 0), 1) * 255);
+    return color + _opacity.toString(16).toUpperCase();
   }
 
   private async autocompleteListener(query: string, args: any): Promise<FlowCard.ArgumentAutocompleteResults> {
---
+++ src/LogNormaliser.ts
@@ -0,0 +1,99 @@
+import dayjs from 'dayjs'
+
+export interface LogValue {
+    t: string,
+    v: number|null
+}
+
+export interface Log {
+    updatesIn: number,
+    values: LogValue[],
+    start: string,
+    end: string,
+    step: number,
+    uri: string,
+    id: string,
+    lastValue: LogValue
+}
+
+class LogNormaliser {
+    public logs: Log;
+    public resolution: string;
+
+    constructor(logs: Log, resolution: string) {
+        this.logs = logs;
+        this.resolution = resolution;
+    }
+
+    public getNormalisedLogs(): LogValue[] {
+        let labelFormat = 'HH:MM';
+        let loopFormat = 'YYYY-MM-DD HH:mm:ss';
+
+        switch (this.resolution) {
+            default:
+            case 'lastHour':
+            case 'last6Hours':
+                loopFormat = 'YYYY-MM-DD HH:mm';
+                labelFormat = 'HH:mm';
+                break;
+            case 'last24Hours':
+            case 'yesterday':
+            case 'today':
+                loopFormat = 'YYYY-MM-DD HH';
+                labelFormat = 'HH:00';
+                break;
+            case 'last14Days':
+            case 'last31Days':
+            case 'lastMonth':
+            case 'thisMonth':
+                loopFormat = 'YYYY-MM-DD';
+                labelFormat = 'DD MMM';
+                break;
+            case 'last7Days':
+            case 'thisWeek':
+            case 'lastWeek':
+                loopFormat = 'YYYY-MM-DD';
+                labelFormat = 'ddd';
+                break;
+            case 'thisYear':
+            case 'lastYear':
+            case 'last2Years':
+                loopFormat = 'YYYY-MM';
+                labelFormat = 'MMM YYYY';
+        }
+
+        const combinedLogs = this.logs.values.reduce((acc: {[key: string]: LogValue[]}, log: LogValue) => {
+            const date = dayjs(log.t);
+            const dateFormatted = date.format(loopFormat);
+
+            if (!acc[dateFormatted]) {
+                acc[dateFormatted] = [];
+            }
+
+            acc[dateFormatted].push(log);
+            return acc;
+        }, {});
+
+        // Get the highest values and format labels
+        const values: LogValue[] = [];
+        Object.keys(combinedLogs).forEach((key) => {
+            const logs = combinedLogs[key];
+            const highestLog = logs.reduce((acc: LogValue, log: LogValue) => {
+                if ((log.v ?? 0) > (acc.v ?? 0)) {
+                    return log;
+                }
+
+                return acc;
+            }, {t: '', v: -999999});
+
+            values.push({
+                t: dayjs(highestLog.t).format(labelFormat),
+                v: highestLog.v
+            });
+        });
+
+        return values;
+    }
+}
+
+export default LogNormaliser;
